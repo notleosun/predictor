1476aee367286828b931a259e2fac9339d2e609f
leo “ning” sun <88621543+notleosun@users.noreply.github.com>
2022-11-10 11:19:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ options = st.sidebar.selectbox(
     ("Starting Page", "Demo page (used in pitching video only)", "Main Program")
 )
 if options == "Starting Page":
-    st.title("RainTomorrow - Eton College Environmental Hackathon")
-    st.header("A machine-learning algorithm that uses past weather data to try and predict extreme downpour, which is made more prominent by climate change.")
+    st.title("Predictor - Eton College Environmental Hackathon")
+    st.header("A machine-learning algorithm that uses past data and predicts possibility of natural disasters, which is made more prominent by climate change.")
     st.subheader("Made by Leo Sun (NPTL)")
 if options == "Main Program":
     st.file_uploader(label = "Upload your .csv file here: ")
