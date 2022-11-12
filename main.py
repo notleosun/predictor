e4617ad0897e030f61dc2e0fbcddf705e384794f
leo “ning” sun <88621543+notleosun@users.noreply.github.com>
2022-11-12 17:09:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ if options == "Starting Page":
     st.subheader("Made by Leo Sun (NPTL)")
 if options == "Main Program":
     uploaded_file = st.file_uploader(label = "Upload your .csv file here: ")
-    bytes_data = uploaded_file.read()
-    fig = px.corr(bytes_data)
+   
+    fig = px.corr(uploaded_file)
     st.plotly_chart(fig, use_container_width=True)
