9ad56891eed25c87e3349a2ba3b5abe9b8387894
leo “ning” sun <88621543+notleosun@users.noreply.github.com>
2022-11-13 20:05:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,38 +17,38 @@ from sklearn.compose import ColumnTransformer
 
 def make_prediction(df, estimator, features_to_fit):
 
-    # Create our target and labels
-    X = df[features_to_fit]
-    y = df["RainTomorrow"]
-    #Identifying Numeric and categorical variables
-    cat_vars = X.select_dtypes(include = ['object','category']).columns
-    num_vars = X.select_dtypes(include = ['number'],exclude=['category']).columns
+	# Create our target and labels
+	X = df[features_to_fit]
+	y = df["RainTomorrow"]
+	#Identifying Numeric and categorical variables
+	cat_vars = X.select_dtypes(include = ['object','category']).columns
+	num_vars = X.select_dtypes(include = ['number'],exclude=['category']).columns
 
-    # Create training and testing data sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, 
-        random_state=43) 
-    # Making the Pipeline
-    numeric_transformer = Pipeline(steps=[("scaler", StandardScaler())])
-    categorical_transformer = Pipeline(steps=[("onehot", OneHotEncoder(handle_unknown="ignore"))])
-    transformer = ColumnTransformer(transformers=[
-        ('num', numeric_transformer, num_vars),
-        ('cat', categorical_transformer, cat_vars)])
-    pipe = Pipeline(steps=[
-        ("transformer", transformer),
-        ("clf", estimator)])
-    # Fit the regressor with the full dataset to be used with predictions
-    pipe.fit(X_train, y_train)
+	# Create training and testing data sets
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, 
+	random_state=43) 
+	# Making the Pipeline
+	numeric_transformer = Pipeline(steps=[("scaler", StandardScaler())])
+	categorical_transformer = Pipeline(steps=[("onehot", OneHotEncoder(handle_unknown="ignore"))])
+	transformer = ColumnTransformer(transformers=[
+	('num', numeric_transformer, num_vars),
+	('cat', categorical_transformer, cat_vars)])
+	pipe = Pipeline(steps=[
+	("transformer", transformer),
+	("clf", estimator)])
+	# Fit the regressor with the full dataset to be used with predictions
+	pipe.fit(X_train, y_train)
 
-    # Do ten-fold cross-validation and compute our average accuracy
-    cv = cross_val_score(pipe, X_test, y_test, cv=10)
-    st.write(f"The accuracy is {cv.mean()}")
+	# Do ten-fold cross-validation and compute our average accuracy
+	cv = cross_val_score(pipe, X_test, y_test, cv=10)
+	st.write(f"The accuracy is {cv.mean()}")
 
-    # Predict today's result
-    X_new = df[features_to_fit]
-    prediction = pipe.predict(X_new)
+	# Predict today's result
+	X_new = df[features_to_fit]
+	prediction = pipe.predict(X_new)
 
-    # Return the predicted result
-    return prediction
+	# Return the predicted result
+	return prediction
         
 train = pd.read_csv("cleaned_train.csv")
 train = train.drop(['Unnamed: 0'], axis = 1)
@@ -59,16 +59,16 @@ option = st.sidebar.selectbox("Navigation",
 )
 
 if option == "Starting Page":
-    st.title("Predictor - Eton College Environmental Hackathon")
-    st.header("A machine-learning algorithm that uses past data and predicts possibility of natural disasters, which is made more prominent by climate change.")
-    st.subheader("Made by Leo Sun (NPTL)")
+	st.title("Predictor - Eton College Environmental Hackathon")
+	st.header("A machine-learning algorithm that uses past data and predicts possibility of natural disasters, which is made more prominent by climate change.")
+	st.subheader("Made by Leo Sun (NPTL)")
 if option == "Main Program (demo)":
-    st.write("This will be the dataset used in this demo.")
-    st.write(train)
-    st.write("Here is a correlation graph between columns of this dataset.")
-    fig = px.imshow(train.corr())
-    st.plotly_chart(fig, use_container_width=True)
-    labels = st.multiselect("What are the labels required for the model", train.columns)
+	st.write("This will be the dataset used in this demo.")
+	st.write(train)
+	st.write("Here is a correlation graph between columns of this dataset.")
+	fig = px.imshow(train.corr())
+	st.plotly_chart(fig, use_container_width=True)
+	labels = st.multiselect("What are the labels required for the model", train.columns)
 	if labels is not None:
 		eee = make_prediction(train, LogisticRegression(), labels)
 		pre_v_res = pd.DataFrame({
