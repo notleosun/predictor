d2b6dad7762097c60c1f99b63e57e2ceaf05785b
leo “ning” sun <88621543+notleosun@users.noreply.github.com>
2022-11-13 19:59:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -70,11 +70,11 @@ if option == "Main Program (demo)":
     st.plotly_chart(fig, use_container_width=True)
     labels = st.multiselect("What are the labels required for the model", train.columns)
     if labels is not None:
-        eee = make_prediction(train, LogisticRegression(), labels)
-        pre_v_res = pd.DataFrame({
+	eee = make_prediction(train, LogisticRegression(), labels)
+	pre_v_res = pd.DataFrame({
 	"Location": train["Location"],
-        "Actual Rain Status": train["RainTomorrow"],
-        "Predicted Rain Status": eee})
+	"Actual Rain Status": train["RainTomorrow"],
+	"Predicted Rain Status": eee})
 	st.write("Here is the predicted results against the actual results.")
-        st.write(pre_v_res)
+	st.write(pre_v_res)
 	st.balloons()
